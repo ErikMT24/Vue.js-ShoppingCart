a5e485a5451ce75d7a17346c6433a30c6bb9a655
erikmoratrejo <erikmoratrejo@gmail.com>
2024-03-05 12:04:35 -0600

    ⭐Feat: App.vue Add destructuring to list

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,12 @@
 import { ref } from 'vue'
 // Creando una referencia reactiva
 // de tipo string
-const header = ref(' App lista de compras');
-const bag = ref('<i class="material-icons shopping-cart-icon">local_mall</i>')
-//const shoppingIcon = ref('material-icons shopping-cart-icon');
+const header = ref (' App lista de compras');
+//const bag = ref('<i class="material-icons shopping-cart-icon">local_mall</i>')
+const shoppingIcon = ref('material-icons shopping-cart-icon');
 //Creando una referencia reactiva
 //para almacenar el valor de la lista 
-const items = ref([ '', 
+const items = ref([ 
 {id: 0, label:'Leche'},
 {id: 2, label:'Arroz'},
 {id: 3, label:'Carne'},
@@ -21,9 +21,8 @@ const items = ref([ '',
 <template>
    <h1> <i :class="shoppingIcon">local_mall</i> <span v-html="bag"></span> {{ header }} </h1>
    <ul>
-    <li v-for="item in items" v-bind:key="item.id">⭐{{ item.label }}</li>
+    <li v-for="({id,label}, i) in items" v-bind:key="id">⭐ {{ i }} {{ label }}</li>
    </ul>
 </template>
-
 <style scoped>
 </style>
